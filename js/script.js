59de8896a4a2e226097d56802b15df489f6693e9
Karolina <carolina.milka18@gmail.com>
2021-02-01 16:39:16 +0300

    practice 1

--- js/script.js
+++ js/script.js
@@ -21,17 +21,44 @@ let arr = ['zero', 'first', 2, {},
 ];
 console.log(arr[1]);
 
-alert('Helo');
+// alert('Helo');
 
-const result = confirm('Do you agree?');
+// const result = confirm('Do you agree?');
 
 
-const answer = prompt('Вам есть 18?', '');
-console.log(answer);
+// const answer = prompt('Вам есть 18?', '');
+// console.log(answer);
 
-const answers = [];
+// const answers = [];
 
-answers[0] = prompt('Name', '');
-answers[1] = prompt('Age', '');
-answers[2] = prompt('City', '');
-console.log(typeof(answers));
+// answers[0] = prompt('Name', '');
+// answers[1] = prompt('Age', '');
+// answers[2] = prompt('City', '');
+// console.log(typeof(answers));
+
+
+// операторы
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+// значения не изменятся
+console.log(incr++);
+console.log(decr--);
+
+// значения изменятся
+console.log(++incr);
+console.log(--decr);
+
+// == сравнение по значениями (2*4 == '8')
+// === сравнение по типу данных (2*4 === 8)
+
+const isChecked = true,
+    isClose = true;
+// и то, и то
+console.log(isChecked && isClose);
+// или то, или то
+console.log(isChecked || isClose);
